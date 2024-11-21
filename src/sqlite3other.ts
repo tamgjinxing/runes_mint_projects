ad0b99320c26025bf7197c790c7fc2fd67725b8b
AllenDon <>
2024-11-21 17:39:59 +0800

    [allen] update sqlite3other

--- src/sqlite3other.ts
+++ src/sqlite3other.ts
@@ -1,17 +1,33 @@
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
+import logger from "./logger";
 
-const db = new sqlite3.Database(global.config.referMintDBPath, (err: Error | null) => {
-    if (err) {
-        console.error('连接数据库失败1：', err.message);
-    } else {
-        console.log('成功连接到 SQLite 数据库');
+let referDB: sqlite3.Database | null = null;
+
+// 初始化数据库连接
+export const initializeReferDB = (): void => {
+    const path = global.config.referMintDBPath;
+    logger.info("Database path:", path);
+
+    referDB = new sqlite3.Database(path, (err: Error | null) => {
+        if (err) {
+            logger.error("Failed to connect to database:", err.message);
+        } else {
+            logger.info("Successfully connected to SQLite database");
+        }
+    });
+};
+
+const getDB = (): sqlite3.Database => {
+    if (!referDB) {
+        throw new Error("Database not initialized. Call initializeDB() first.");
     }
-});
+    return referDB;
+};
 
 // 更新状态函数
 const updateStatus = (quote: string, isPaid: number, state: string, nowTime: number): void => {
     const sql = `UPDATE mint_quotes SET state = ?, paid_time = ?, paid=? WHERE quote = ?`;
-    db.run(sql, [state, nowTime, isPaid, quote], function (this: sqlite3.RunResult, err: Error | null) { // 为 err 指定类型
+    getDB().run(sql, [state, nowTime, isPaid, quote], function (this: sqlite3.RunResult, err: Error | null) { // 为 err 指定类型
         if (err) {
             console.error('更新数据失败：', err.message);
         } else {
